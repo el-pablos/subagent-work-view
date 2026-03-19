f1d452cfa4dc5d1cc3908a186bf17f62d8c782e0
el-pablos <yeteprem.end23juni@gmail.com>
2026-03-19 21:24:22 +0700

    update: auto-commit perubahan terbaru
    el-pablos <yeteprem.end23juni@gmail.com>

--- backend/app/Http/Controllers/Api/MessageController.php
+++ backend/app/Http/Controllers/Api/MessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MessageResource;
+use App\Jobs\BatchStoreMessages;
 use App\Models\Message;
 use App\Models\Session;
 use App\Events\MessageCreated;
