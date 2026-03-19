cda6400f291ab3dfe2c801ce05277f0043e8faba
el-pablos <yeteprem.end23juni@gmail.com>
2026-03-19 21:27:27 +0700

    add: export ProgressBar dan ProgressSteps di common components
    Menambahkan export untuk komponen progress animations:
    - ProgressBar dengan variant default/striped/gradient
    - ProgressSteps untuk step indicator
    
    el-pablos <yeteprem.end23juni@gmail.com>

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -1,5 +1,7 @@
 export { default as StatusChip } from "./StatusChip";
 export { default as ProgressRing } from "./ProgressRing";
+export { default as ProgressBar } from "./ProgressBar";
+export { default as ProgressSteps } from "./ProgressSteps";
 export { default as AlertToast, AlertToastManager } from "./AlertToast";
 export { default as Skeleton } from "./Skeleton";
 export { default as RelativeTime } from "./RelativeTime";
@@ -8,8 +10,16 @@ export { default as Timestamp } from "./Timestamp";
 export { default as Avatar } from "./Avatar";
 export { default as AvatarGroup } from "./AvatarGroup";
 export { default as Tooltip } from "./Tooltip";
+export { default as SearchInput } from "./SearchInput";
+export { default as FilterTags } from "./FilterTags";
 export type { ToastMessage, AlertType } from "./AlertToast";
 export type { StatusType } from "./StatusChip";
+export type {
+  ProgressBarVariant,
+  ProgressBarSize,
+  ProgressBarColor,
+} from "./ProgressBar";
+export type { Step, StepStatus } from "./ProgressSteps";
 export type { SkeletonProps } from "./Skeleton";
 export type { RelativeTimeProps } from "./RelativeTime";
 export type { DurationProps } from "./Duration";
@@ -22,3 +32,4 @@ export type {
 } from "./Avatar";
 export type { AvatarGroupProps } from "./AvatarGroup";
 export type { TooltipPosition } from "./Tooltip";
+export type { FilterTag } from "./FilterTags";
